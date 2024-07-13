5dd91ffece26f8096cd72004e51c81739b5241bb
CharityNambi05 <charity.nambi@icloud.com>
2024-07-13 23:24:12 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Your code here
+// Your code hereconst DODGER 
+
+
+const DODGER = document.getElementById('DODGER');
+
+function moveDodgerLeft() {
+  const currentLeft = parseInt(DODGER.style.left) || 0;
+  const newLeft = Math.max(0, currentLeft - 10); 
+  DODGER.style.left = `${newLeft}px`;
+}
+
+function moveDodgerRight() {
+  const currentLeft = parseInt(DODGER.style.left) || 0;
+  const newLeft = Math.min(window.innerWidth - DODGER.offsetWidth, currentLeft + 10); 
+  DODGER.style.left = `${newLeft}px`;
+}
